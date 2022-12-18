1986534e94bf18970ec3ef41d7081bfd1ad84248
Winona Gaile Deona <115537613+winonagailedeona@users.noreply.github.com>
2022-12-18 18:02:42 +0800

    Update continentea.sql

--- continentea.sql
+++ continentea.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 14, 2022 at 01:24 AM
+-- Generation Time: Dec 18, 2022 at 10:02 AM
 -- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -66,7 +66,18 @@ CREATE TABLE IF NOT EXISTS `cart` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `user_id_2` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `prod_name`, `image`, `price`, `quantity`, `size`, `total`) VALUES
+(37, 3, 'Nachos', 'img/menu/nachos.jpg', 120, 5, 0, 600),
+(35, 3, 'Nachos', 'img/menu/nachos.jpg', 120, 5, 0, 600),
+(36, 3, 'Chili Roll', 'img/menu/chili roll.jpg', 120, 4, 0, 480),
+(38, 3, 'Nachos', 'img/menu/nachos.jpg', 120, 5, 0, 600),
+(39, 3, 'with Pork Adobo', 'img/menu/pork adobo.jpg', 160, 10, 0, 1600);
 
 -- --------------------------------------------------------
 
@@ -87,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
@@ -99,16 +110,20 @@ INSERT INTO `product` (`id`, `name`, `description`, `price`, `quantity`, `size`,
 (9, 'Baked Macaroni', 'Mac and cheese is a rich and creamy dish consisting of macaroni pasta mixed with a cheesy sauce.', 100, 10, '', 'img/menu/baked macaroni.jpg', 'pasta', '2022-12-07 20:55:45', '0000-00-00 00:00:00'),
 (12, 'Chicken Burger', 'A sandwich consisting of a patty made from ground chicken, served in a bun, typically hot and often with other ingredients.', 150, 10, '', 'img/menu/chicken burger.jpg', 'sandwich', '2022-12-07 20:59:19', '0000-00-00 00:00:00'),
 (14, 'Lumpiang Shanghai', 'Filipino-style spring rolls made of ground pork, chicken, or beef filling wrapped in thin crepes called lumpia wrappers.', 80, 10, '0', 'img/menu/lumpiang shanghai.jpg', 'pica', '2022-12-07 21:13:07', '2022-12-07 21:14:31'),
-(15, 'Nachos', 'A tortilla chip topped with melted cheese and often additional savory toppings', 120, 10, '', 'img/menu/nachos.jpg', 'pica', '2022-12-07 21:18:59', '2022-12-07 21:19:44'),
-(16, 'Chili Roll', 'It is a type of lumpia and it is commonly eaten as an appetizer or as a companion to beer.', 120, 10, '', 'img/menu/chili roll.jpg', 'pica', '2022-12-07 21:21:22', '0000-00-00 00:00:00'),
-(17, 'Chicken Cordon Bleu Bites', 'French-inspired poultry dish, although evidence suggests that it was actually developed in the United States by chefs imitating other stuffed meat dishes from Europe.', 120, 10, '', 'img\\menu\\menu\\chicken cordon bleu.jpg\r\n', 'pica', '2022-12-07 21:27:33', '2022-12-07 21:28:05'),
+(15, 'Nachos', 'A tortilla chip topped with melted cheese and often additional savory toppings', 120, 5, '', 'img/menu/nachos.jpg', 'pica', '2022-12-07 21:18:59', '2022-12-08 16:18:09'),
+(16, 'Chili Roll', 'It is a type of lumpia and it is commonly eaten as an appetizer or as a companion to beer.', 120, 4, '', 'img/menu/chili roll.jpg', 'pica', '2022-12-07 21:21:22', '2022-12-08 16:18:22'),
+(17, 'Chicken Cordon Bleu Bites', 'French-inspired poultry dish, although evidence suggests that it was actually developed in the United States by chefs imitating other stuffed meat dishes from Europe.', 120, 2, '', 'img\\menu\\menu\\chicken cordon bleu.jpg\r\n', 'pica', '2022-12-07 21:27:33', '2022-12-08 16:18:36'),
 (18, 'with fried Chicken', 'A dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried.', 160, 10, '', 'img\\menu\\friedch.jpg', 'meals', '2022-12-07 21:36:00', '0000-00-00 00:00:00'),
 (19, 'with Pork Adobo', 'It is a dish composed of pork slices cooked in soy sauce, vinegar, and garlic. ', 160, 10, '', 'img/menu/pork adobo.jpg', 'meals', '2022-12-07 21:39:19', '0000-00-00 00:00:00'),
 (22, 'San Rival Cake', 'A popular Filipino layer cake consisting of cashew meringue and French buttercream.', 140, 10, '', 'img/menu/sans rival cake.jpg', 'dessert', '2022-12-07 21:57:48', '0000-00-00 00:00:00'),
 (23, 'Crinkles', 'A variation of the chocolate cookies where its texture and appearance are its main characteristics, its soft fudge-like chocolate cake in texture and have that distinctive powdered sugar coating.', 100, 10, '', 'img/menu/crinkles.jpg', 'dessert', '2022-12-07 22:04:43', '2022-12-07 22:05:59'),
 (21, 'with Cordon Bleu Bites', 'A dish of meat wrapped around cheese (or with cheese filling), then breaded and pan-fried or deep-fried.', 160, 10, '', 'img/menu/menu/chicken cordon bleu.jpg', 'meals', '2022-12-07 21:53:35', '0000-00-00 00:00:00'),
 (24, 'Carbonara', 'An Italian sauce for pasta which appears to have emerged at some point after the Second World War.', 160, 10, '', 'img/menu/carbonara.jpg', 'pasta', '2022-12-07 22:26:43', '0000-00-00 00:00:00'),
-(25, 'Hotdog Sandwich', 'A food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. ', 70, 10, '', 'img/menu/hotdog sandwich.jpg', 'sandwich', '2022-12-07 22:29:24', '0000-00-00 00:00:00');
+(25, 'Hotdog Sandwich', 'A food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. ', 70, 10, '', 'img/menu/hotdog sandwich.jpg', 'sandwich', '2022-12-07 22:29:24', '0000-00-00 00:00:00'),
+(26, 'Wintermelon Milk Tea', 'Wintermelon Milk Tea has a very light and fresh flavour that\'s almost a bit grassy. The closest flavour match would be fresh cucumber.', 95, 10, '', 'img/menu/winter.jpg', 'milktea', '2022-12-18 17:42:52', '2022-12-18 17:46:35'),
+(27, 'Hokkaido Milk Tea', 'Hokkaido milk tea is earthy and sweet, but its authentic flavours depend on specific tea and sweetener ingredients. ', 105, 10, '', 'img/menu/hokkaido.jpg', 'milktea', '2022-12-18 17:49:06', '0000-00-00 00:00:00'),
+(28, 'Matcha Milk Tea', 'Matcha has a bright, vegetal, and slightly bitter taste, with notes of sweetness and umami. High quality ceremonial grade matcha is suitable to drink on its own, while culinary grade matcha can be added to lattes, smoothies, and more.', 105, 10, '', 'img/menu/matcha.jpg', 'milktea', '2022-12-18 17:49:57', '0000-00-00 00:00:00'),
+(29, 'Americano Coffee', 'An Americano is made by pouring hot water over one or two espresso shots, resulting in a drink of similar volume and strength to regular coffee.', 115, 10, '', 'img/menu/americano.jpg', 'milktea', '2022-12-18 18:00:16', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
