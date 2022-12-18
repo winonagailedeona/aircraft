0132b16a3a285c6b966a9d504384dcfd703256f5
Paulyn Familara <paulynfamilara91@gmail.com>
2022-12-18 01:14:44 -0800

    commit

--- aircraft/app/Config/App.php
+++ aircraft/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/aircraft_continenteaCafe/';
+    public $baseURL = 'http://localhost/aircraft/';
 
     /**
      * --------------------------------------------------------------------------
